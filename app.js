32512e306ce20725084b85dbd25855d8ceea2dd4
Michael Antczak <Michael.Antczak@gmail.com>
2017-06-16 08:18:01 +0100

    Console.log data on POST

--- app.js
+++ app.js
@@ -17,15 +17,16 @@ var server = http.createServer(function (req, res) {
 
         req.on('end', function() {
             if (req.url === '/') {
-                log('Received message: ' + body);
+                console.log('Received message: ' + body);
             } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+                console.log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
 
             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
             res.end();
         });
     } else {
+        console.log("DUPA");
         console.log("URL : ", req.url);
         res.writeHead(200);
         res.write(html);
