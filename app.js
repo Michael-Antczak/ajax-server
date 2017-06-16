8a067762a672f100203021083552191fa7c39411
Michael <Michael.Antczak@gmail.com>
2017-06-16 19:19:57 +0100

    POST up

--- app.js
+++ app.js
@@ -10,50 +10,50 @@ var myLastMessage = [];
 var server = http.createServer(function (req, res) {
 
 //     // dealing with POST request
-//     if (req.method === 'POST') {
-//         var body = '';
+    if (req.method === 'POST') {
+        var body = '';
 
-//         // get the content of the body
-//         req.on('data', function(chunk) {
-//             body += chunk;
-//         });
+        // get the content of the body
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
 
-//         // when we are done with body
-//         req.on('end', function() {
+        // when we are done with body
+        req.on('end', function() {
 
-//             // get the id parameter from the url
-//             var query = url.parse(req.url).query;
-//             var id = query.replace('id=', '');
+            // get the id parameter from the url
+            var query = url.parse(req.url).query;
+            var id = query.replace('id=', '');
 
-//             // create the message object
-//             var result = {
-//                 id: id,
-//                 body: body, 
-//             }
+            // create the message object
+            var result = {
+                id: id,
+                body: body, 
+            }
 
-//             // set the flag for checking if there is another message with the same id
-//             var found = false;
+            // set the flag for checking if there is another message with the same id
+            var found = false;
 
-//             // look for the last message for given id, if found -> replace, 
-//             // not found then just add
-//             myLastMessage.find(function(element, index, array)  {
+            // look for the last message for given id, if found -> replace, 
+            // not found then just add
+            myLastMessage.find(function(element, index, array)  {
                 
-//                 if(element.id === id) {
-//                     found = true;
-//                     myLastMessage.splice(index, 1, result);
+                if(element.id === id) {
+                    found = true;
+                    myLastMessage.splice(index, 1, result);
 
-//                 }
+                }
 
-//             });
+            });
 
-//             // at this point the id is not found 
-//             if(!found) myLastMessage.push(result);
-//             console.log(myLastMessage);
+            // at this point the id is not found 
+            if(!found) myLastMessage.push(result);
+            console.log(myLastMessage);
 
-//             res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
-//             res.end("\n\nSuccess");
+            res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
+            res.end("\n\nSuccess");
 
-//         });  // end of POST request
+        });  // end of POST request
         
 //     // dealing with GET request
 // } else if (req.method === 'GET') {
@@ -79,12 +79,12 @@ var server = http.createServer(function (req, res) {
 //             res.end();
 
 //     // 
-//     } else {
+    } else {
        
         res.writeHead(200);
         res.write(html);
         res.end();
-//     }
+    }
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
