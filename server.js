0e8e6e98fa9a5f81f57ac162018d6298e08b033d
Michael Antczak <Michael.Antczak@gmail.com>
2017-06-16 02:11:02 +0100

    Update server.js with Express

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-const http = require('http'); 
+var express    = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+// parse application/json
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.listen(2222, function() {
+    console.log("Listening on 2222.");
+});
+
+app.post("/", function (req, res) {
+  
+  console.log(req.body) 
+  res.send(200, req.body);
+  
+});
+
+app.get("/", function (req, res) {
+
+    var time = new Date();
+    time = "Date now is " + time;
+  
+    var obj = {
+        message: time
+    }
+    
+  res.send(200, obj);
+});
